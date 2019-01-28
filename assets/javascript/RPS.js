9f5836293259e4549870fb15688632703f0068eb
Jeff Levine <jlevine_84@yahoo.com>
2019-01-28 14:25:20 -0500

    initial styling and asset creations

---
+++ assets/javascript/RPS.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBiVJN6P7baMD2cRXaGhyRS4Vv0wju5vjA",
+    authDomain: "rpsls-1b10e.firebaseapp.com",
+    databaseURL: "https://rpsls-1b10e.firebaseio.com",
+    projectId: "rpsls-1b10e",
+    storageBucket: "",
+    messagingSenderId: "5634337018"
+};
+firebase.initializeApp(config);
+database = firebase.database();
